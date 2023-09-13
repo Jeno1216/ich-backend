cdcd0693f74167e8c3a8e8eb28d701e84d98f23b
Jeno Bellido <jeno.bellido@wvsu.edu.ph>
2023-09-13 16:26:09 +0800

    asda

--- index.js
+++ index.js
@@ -107,7 +107,9 @@ app.post('/login', (req, res) => {
                             'jwt-secret-key',
                             { expiresIn: '1d' }
                         );
-                        res.cookie('token', token, { sameSite: 'None', secure: true });
+                        res.cookie('token', token, { sameSite: 'None', secure: true }); 
+                        // sameSite none because our frontend has different domain with backend. secure true because
+                        // the deployed frontend and server is https not http
                         return res.json("Success");
                     } else {
                         return res.json('Password incorrect');
