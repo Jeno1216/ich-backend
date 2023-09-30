1b680e5389bd276e93cd8da161300a6f307550f3
Jeno Bellido <jeno.bellido@wvsu.edu.ph>
2023-09-30 22:43:19 +0800

    asda

--- routes/auth.js
+++ routes/auth.js
@@ -2,32 +2,15 @@ const router = require('express').Router();
 const passport = require('passport');
 
 router.get('/login/success', (req, res) => {
-    if (req.user) {
-        const token = jwt.sign(
-            { _id: req.user._id, username: req.user.username, email: req.user.email },
-            'jwt-secret-key',
-            { expiresIn: '1d' }
-        );
-
-        // Set a cookie with the token (similar to /logingoogle)
-        res.cookie('token', token, {
-            sameSite: 'None',
-            secure: true,
-        });
-
+    if(req.user){
         res.status(200).json({
             success: true,
             message: 'successfully logged',
             user: req.user,
-        });
-    } else {
-        res.status(401).json({
-            success: false,
-            message: 'failure',
-        });
+            // cookies: req.cookies
+        });    
     }
 });
-
   router.get('/login/failed', (req, res) => {
     res.status(401).json({
         success: false,
